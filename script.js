5c0eaacfe070fe1ff8a4bc8c5622336eb0df0936
Gabriel Santos <gabriel.santos@edu.unipar.com>
2021-03-23 22:14:47 -0300

    Aula 03 incompleta

--- script.js
+++ script.js
@@ -5,8 +5,96 @@ function open() {
 function close() {
   document.querySelector('.modal-overlay').classList.toggle('active');
 }
-const Modal = { open, close }
+function toggle() {
+  document.querySelector('.modal-overlay').classList.toggle('active');
+}
+const Modal = { open, close, toggle }
 const newTransaction = document.querySelector('.button.new');
 const cancel = document.querySelector('.button.cancel')
-newTransaction.addEventListener('click', Modal.open);
-cancel.addEventListener('click', Modal.close);
+newTransaction.addEventListener('click', Modal.toggle);
+cancel.addEventListener('click', Modal.toggle);
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: 50000,
+    date: '23/03/2021'
+  },
+  {
+    id: 2,
+    description: 'Luz',
+    amount: 5000,
+    date: '23/03/2021'
+  },
+  {
+    id: 3,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/03/2021'
+  },
+  {
+    id: 4,
+    description: 'Luz',
+    amount: 5000,
+    date: '23/03/2021'
+  },
+  {
+    id: 5,
+    description: 'Luz',
+    amount: -5000,
+    date: '23/03/2021'
+  },
+];
+const Transaction = {
+  incomes() {
+
+  },
+  expenses() {
+
+  },
+  total() {
+
+  }
+}
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    DOM.transactionsContainer.appendChild(tr)
+
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
+
+    const amount = Utils.formatCurrency(transaction.amount);
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${CSSclass}">${amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td>
+      <img src="./assets/minus.svg" alt="Remover Transação">
+    </td>
+    `;
+    return html;
+  }
+}
+const Utils = {
+  formatCurrency(value) {
+    console.log(value);
+    const signal = Number(value) < 0 ? "-" : "";
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value;
+
+  }
+}
+transactions.forEach((item) => {
+  DOM.addTransaction(item);
+});
